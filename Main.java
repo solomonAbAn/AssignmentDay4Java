3c790a060469e87dd77fbc8cf6561d63944f5588
Sleiman Abou antoun <sleimen.aa@gmail.com>
2023-12-20 15:00:07 -0500

    first commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int i;
+
+
+        for (i = 0; i <= 5; i++) {
+
+            System.out.println(i);
+
+
+        }
+
+
+    }
+}
